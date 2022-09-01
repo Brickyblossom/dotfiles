1306663e87c7cbdf3e8496db465dfb5e6e6c58b1
brickyblossom <tuattit9373@gmail.com>
2022-09-01 20:49:05 +0700

    Fix echo

--- setup.sh
+++ setup.sh
@@ -1,23 +1,23 @@
 cd ..
 
-echo ("Updating apt")
+echo "Updating apt"
 apt update
 
-echo ("Installing xinit...")
+echo "Installing xinit..."
 apt install xinit -y
 
-echo ("Installing i3...")
+echo "Installing i3..."
 apt install i3 -y
 
-echo ("Creating config directory...")
+echo "Creating config directory..."
 mkdir .config/polybar
 
-echo ("Installing Polybar...")
+echo "Installing Polybar..."
 apt install polybar -y
 
 cd My-Desktop
 
-echo ("Copying Polybar config...")
+echo "Copying Polybar config..."
 cp config.ini .config/polybar/config.ini
 cp launch.sh .config/polybar/launch.sh
 
@@ -27,33 +27,33 @@ cp config ../.config/i3/config
 
 cd ..
 
-echo ("Downloading Polybar themes...")
+echo "Downloading Polybar themes..."
 git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
 
 cd polybar-themes
 
 chmod +x setup.sh
 
-echo ("Setting up Polybar theme...")
+echo "Setting up Polybar theme..."
 sh setup.sh
 
 cd ../My-Desktop
 
-echo ("Installing zsh...")
+echo "Installing zsh..."
 apt install zsh -y
 
-echo ("Installing ohmyzsh...")
+echo "Installing ohmyzsh..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo ("Downloading powerlevel10k...")
+echo "Downloading powerlevel10k..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-echo ("Copying .zshrc config and installing powerlevel10k...")
+echo "Copying .zshrc config and installing powerlevel10k..."
 cp .zshrc ../.zshrc
 
-echo ("Launching zsh and starting powerlevel10k config")
+echo "Launching zsh and starting powerlevel10k config"
 zsh
 
 
-echo ("Starting X session...")
+echo "Starting X session..."
 startx
