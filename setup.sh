fd7d7e331209417a9d04bba5160d9b7afefe6210
brickyblossom <tuattit9373@gmail.com>
2022-09-03 14:14:34 +0700

    Fix dependencies

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ echo "Installing xinit..."
 sudo apt install xinit -y
 
 echo "Building i3-gaps..."
-sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool libxcb-shape0-dev -y 
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake -y
 
 mkdir temp
 cd temp
