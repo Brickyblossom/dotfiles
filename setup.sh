a0e743ac8fdf11ef2024ba7514743701ed7bbda2
brickyblossom <tuattit9373@gmail.com>
2022-09-03 13:34:03 +0700

    Install i3-gaps by building from source since there aren't any PPA for Ubuntu 22

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+dir = $(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
 cd ..
 
 echo "Updating apt"
@@ -7,8 +9,21 @@ sudo apt install python3-pip
 echo "Installing xinit..."
 sudo apt install xinit -y
 
-echo "Installing i3..."
-sudo apt install i3 -y
+echo "Building i3-gaps..."
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool libxcb-shape0-dev 
+cd /tmp
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+autoreconf --force --install
+rm -rf build
+mkdir build
+cd build
+../configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install
+
+cd dir
 
 echo "Creating config directory..."
 mkdir .config/polybar
