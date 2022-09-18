d9b51c7fa36fb4c12b80b4b4bfa71d72b9e1ef85
brickyblossom <tuattit9373@gmail.com>
2022-09-18 23:51:59 +0700

    fix mkdir

--- setup_manjaro_i3.sh
+++ setup_manjaro_i3.sh
@@ -97,6 +97,7 @@ install_font(){
 
 copy_config(){
 	mkdir -p $HOME/.config/polybar
+	mkdir -p $HOME/.config/neofetch
 	cp $HOME/dotfiles/config.ini $HOME/.config/polybar/config.ini
 	cp $HOME/dotfiles/launch.sh $HOME/.config/polybar/launch.sh
 	chmod +x $HOME/.config/polybar/launch.sh
